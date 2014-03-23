16b25f162cdfedc5d6c772f718f73543f0726350
Shelton Gayles <sgbythelake@gmail.com>
2014-03-22 19:22:16 -0700

    basic db action get and delte methods

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 require_once 'core/init.php';
 
+//$user = DB::getInstance()->query('SELECT username FROM usersd WHERE username = ?', array('fred'));
+$user = DB::getInstance()->get('users', array('username', '=', 'alex'));
 $db = DB::getInstance();
+        $db->get('users', array('username', '=', 'alex'));
+if(!$db->count()) {
+    echo ' no user ';
+} else {
+    echo 'success';
+    
+}
